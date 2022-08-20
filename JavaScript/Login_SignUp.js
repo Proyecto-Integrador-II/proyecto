42c468f96d3a3f4992ca7b7218b59dccd654075f
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-08-20 16:55:30 -0500

    Update Login_SignUp.js

--- JavaScript/Login_SignUp.js
+++ JavaScript/Login_SignUp.js
@@ -51,7 +51,7 @@ function register(){
     }else{
         formulario_register.style.display = "block";
         contenedor_login_register.style.left = "0px";
-        formulario_login.style.display = "block";
+        formulario_login.style.display = "none";
         caja_trasera_register.style.display = "none";
         caja_trasera_login.style.display ="block";
         caja_trasera_login.style.opacity = "1";
