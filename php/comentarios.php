0e846e2311ff19f0a218060becc7db7b31780c91
Slisth <79495240+Slisth@users.noreply.github.com>
2022-08-27 19:17:29 -0500

    RESEÑAS

---
+++ php/comentarios.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Comentarios</title>
+    <link rel="stylesheet" href="../CSS/comentarios.css">
+</head>
+<body>
+    <header class="header">
+
+        <div class="ctn-header">
+
+            <label id="si" class="lbl-header">COMENTARIOS</label>
+
+        </div>
+    </header>
+
+    <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v14.0" nonce="cHVtgB4M"></script>
+
+
+
+    <div class="ctn-data">
+        <p class="p-data">
+            Empanadas: 
+        </p>
+
+        <p class="p-data">
+            Hamburguesas:
+        </p>
+
+        <p class="p-data">
+            Galletas:
+        </p>
+        
+
+
+        <div class="ctn-lbl-comentar">
+        <label for="" class="lbl-comentar">Lista de comentarios</label>
+        </div>
+        <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-width="100%" data-numposts="1"></div>
+    </div>
+</body>
+</html>
