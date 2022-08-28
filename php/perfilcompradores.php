d553cb5b8c92eabf8ef82961200419789f5d8f12
Slisth <79495240+Slisth@users.noreply.github.com>
2022-08-28 18:13:56 -0500

    Editar compradores

---
+++ php/perfilcompradores.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/perfilcompradores.css">
+    <title>Editar perfil compradores</title>
+</head>
+<body>
+    <main>
+        <div class="contenedor__editar-producto">
+            <form action="" method="POST" class="">
+                <h2>
+                    Editar Perfil Compradores
+                </h2>
+                <input type="text" placeholder="Nombre" name="Nombre" >
+                <input type="email" placeholder="Correo" name="correo">
+                <label placeholder="Cambiar imagen"><input  type="file" name="imagen"></label>
+                <button type="submit" name="Cambiar" value="Cambiar" id="boton">
+                    Actualizar
+                </button>   
+            </form>
+        </div>
+    </main>
+</body>
+</html>
