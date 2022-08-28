6bc567748b4d3397e6184b9124b97b3284c981b9
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-08-28 01:23:56 -0500

    Squashed commit of the following:
    commit aa45bf93bd949d396380ad88dfb2e3ffd2636766
    Author: Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
    Date:   Sun Aug 28 00:59:20 2022 -0500
    
        Registro de producto

--- php/registrarProducto.php
+++ php/registrarProducto.php
@@ -9,51 +9,51 @@
 </head>
 <body>
     <div class="container">
-            <div class="content">
-				<form name="add_product" id="add_product">
-					<div class="header">						
-						<h4 class="title">Añadir Producto</h4>
+		<div class="content">
+			<form name="add_product" id="add_product">
+				<div class="header">						
+					<h4 class="title">Añadir Producto</h4>
+				</div>
+				<div class="formulario">
+					<div class="form-group">
+						<label>ID</label>
+						<input type="text" id="id" required>
+						
 					</div>
-					<div class="formulario">
-						<div class="form-group">
-							<label>ID</label>
-							<input type="text" id="id" required>
-							
-						</div>
-						<div class="form-group">
-							<label>Nombre</label>
-							<input type="text" id="name" required>
-						</div>
-						<div class="form-group">
-							<label>Categoría</label>
-							<input type="text" id="category" required>
-						</div>
-						<div class="form-group">
-							<label>Cantidad</label>
-							<input type="number" id="stock" required>
-						</div>
-						<div class="form-group">
-							<label>Precio</label>
-							<input type="text" id="price" required>
-						</div>
-                        <div class="form-group">
-							<label>Descripción</label>
-							<input type="text" id="description" required>
-						</div>
-                        <div class="form-group">
-                            <label>Habilitado</label>  
-                            <select id="transporte" >
-                                <option value="1">Si</option>
-                                <option value="2">No</option>
-                            </select>
-						</div>
+					<div class="form-group">
+						<label>Nombre</label>
+						<input type="text" id="name" required>
 					</div>
-					<div class="modal-footer">
-						<input type="button" class="cancelar" value="Cancelar">
-						<input type="submit" class="registrar" value="Guardar datos">
+					<div class="form-group">
+						<label>Categoría</label>
+						<input type="text" id="category" required>
 					</div>
-				</form>
-			</div>
+					<div class="form-group">
+						<label>Cantidad</label>
+						<input type="number" id="stock" required>
+					</div>
+					<div class="form-group">
+						<label>Precio</label>
+						<input type="text" id="price" required>
+					</div>
+					<div class="form-group">
+						<label>Descripción</label>
+						<input type="text" id="description" required>
+					</div>
+					<div class="form-group">
+						<label>Habilitado</label>  
+						<select id="transporte" >
+							<option value="1">Si</option>
+							<option value="2">No</option>
+						</select>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<input type="button" class="cancelar" value="Cancelar">
+					<input type="submit" class="registrar" value="Guardar datos">
+				</div>
+			</form>
+		</div>
     </div>
 </body>
 </html>
