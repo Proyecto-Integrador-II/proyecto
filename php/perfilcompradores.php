8fdda9324a73f37546f75ddb89c46b66336421a1
Slisth <79495240+Slisth@users.noreply.github.com>
2022-08-28 22:32:19 -0500

    Avances-EDITAR COMPRADORES

--- php/perfilcompradores.php
+++ php/perfilcompradores.php
@@ -17,23 +17,39 @@
                 <input type="text" placeholder="Nombre" name="Nombre" >
                 <input type="password" placeholder="Contraseña" name="Contraseña" >
                 <input type="email" placeholder="Correo" name="correo">
-                <button type="submit" name="Cambiar" value="Cambiar" id="boton">
-                    Actualizar
+                <button type="submit" name="limpiardatos" value="Cambiar" id="boton">
+                    Limpiar
                 </button>   
+                <button type="submit" name="grabardatos" value="Cambiar" id="boton">
+                    Grabar
+                </button> 
+                <button type="submit" name="modificardatos" value="Cambiar" id="boton">
+                    Modificar
+                </button> 
+                <button type="submit" name="eliminardatos" value="Cambiar" id="boton">
+                    Eliminar
+                </button> 
             </form>
         </div>
+<?php 
+$sql="SELECT * FROM usuarios";
+$result=mysql_query($conn,$sql);
+
+while($mostrar=mysql_fetch_array($result))
+{
+?>
+<tr> <td><?php echo $mostrar['codigo'] ?>
+	<td><?php echo $mostrar['nombre'] ?>
+	<td><?php echo $mostrar['edad'] ?>
+	<td><?php echo $mostrar['telefono'] ?>
+
+</tr>
+<?php
+}
+
+?>
     </main>
 </body>
 </html>
 
 
-<?php
-if(isset($_POST['cambiar'])){
-    if(!empty($_POST['Nombre'])){
-        UPDATE usuarios 
-
-
-
-    }
-}
-?>
