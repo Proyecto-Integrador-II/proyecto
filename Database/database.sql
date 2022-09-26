b90359422c8874ec5f80f1e29ab3c350e43d88b5
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-09-25 23:04:49 -0500

    Update database.sql

--- Database/database.sql
+++ Database/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3307
--- Tiempo de generación: 21-09-2022 a las 04:29:33
+-- Tiempo de generación: 26-09-2022 a las 06:02:23
 -- Versión del servidor: 10.6.7-MariaDB
 -- Versión de PHP: 8.1.1
 
@@ -24,16 +24,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `lugares`
+--
+
+CREATE TABLE `lugares` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Volcado de datos para la tabla `lugares`
+--
+
+INSERT INTO `lugares` (`id`, `nombre`) VALUES
+(2, 'Delacuesta'),
+(3, 'Parque caracoli'),
+(1, 'UPB');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `producto`
 --
 
 CREATE TABLE `producto` (
   `codproducto` int(11) NOT NULL,
-  `descripcion` varchar(100) DEFAULT NULL,
-  `proveedor` int(11) DEFAULT NULL,
-  `precio` decimal(10,2) DEFAULT NULL,
-  `existencia` int(11) DEFAULT NULL,
-  `foto` text DEFAULT NULL
+  `nombre` varchar(100) DEFAULT NULL,
+  `precio` int(10) DEFAULT NULL,
+  `lugar` int(11) DEFAULT NULL,
+  `descripcion` varchar(1000) DEFAULT NULL,
+  `foto` text DEFAULT NULL,
+  `proveedor` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -74,15 +95,30 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idusuario`, `nombre`, `apellido`, `correo`, `clave`, `rol`, `habilitado`, `calificacion`) VALUES
+(10, 'Jean pierre', 'Parra Suarez', 'jean@administrador.com', 'Jeanparra17$', 3, 1, 0);
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `lugares`
+--
+ALTER TABLE `lugares`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre_unique` (`nombre`);
 
 --
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`codproducto`),
-  ADD KEY `proveedor` (`proveedor`);
+  ADD KEY `proveedor` (`proveedor`),
+  ADD KEY `lugar` (`lugar`);
 
 --
 -- Indices de la tabla `rol`
@@ -102,6 +138,12 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `lugares`
+--
+ALTER TABLE `lugares`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
@@ -111,7 +153,7 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
@@ -121,6 +163,7 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `producto`
 --
 ALTER TABLE `producto`
+  ADD CONSTRAINT `FK_producto_lugares` FOREIGN KEY (`lugar`) REFERENCES `lugares` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`proveedor`) REFERENCES `usuario` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
