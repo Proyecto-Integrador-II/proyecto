5e790b827c5ff4b70d8872645a3b6580f50644a6
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-09-28 20:46:05 -0500

    Create conexion.php

---
+++ php/todos/conexion.php
@@ -0,0 +1,12 @@
+<?php
+    $host = 'localhost:3307';
+    $user = 'root';
+    $password = 'upb2021';
+    $db = 'proyecto2';
+
+    $conection = @mysqli_connect($host,$user,$password,$db);
+
+    if(!$conection){
+        echo "Error en la conexión";
+    }
+?>
