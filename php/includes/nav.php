30b83e018d6b756cb3c40f618244fa81acbf5def
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-09-28 20:45:32 -0500

    Create nav.php

---
+++ php/includes/nav.php
@@ -0,0 +1,22 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+    <?php 
+        if($_SESSION['user'] == 1){
+        ?>
+        <li class="principal">
+
+            <a href="#">Usuarios</a>
+            <ul>
+                <li><a href="lista_usuarios.php">Lista de Usuarios</a></li>
+            </ul>
+        </li>
+    <?php } ?>
+        <li class="principal">
+            <a href="#">Productos</a>
+            <ul>
+                <li><a href="#">Lista de Productos</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
