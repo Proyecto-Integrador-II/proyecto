5e9643fd9125855d39b338bb4a082f1c1aa5d31e
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-09-28 20:45:38 -0500

    Create scripts.php

---
+++ php/includes/scripts.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="../../CSS/style.css">
+<link rel="stylesheet" type="text/css" href="../../CSS/datos.css">
+<link href="../../fontawesome-free-6.2.0-web/css/fontawesome.css" rel="stylesheet"> 
+<link href="../../fontawesome-free-6.2.0-web/css/brands.css" rel="stylesheet">
+<link href="../../fontawesome-free-6.2.0-web/css/solid.css" rel="stylesheet">
+<link href="../../fontawesome-free-6.2.0-web/css/v5-font-face.css" rel="stylesheet">
+<script type="text/javascript" src="../../JavaScript/jquery.min.js"></script>
+<script type="text/javascript" src="../../JavaScript/functions.js"></script>
