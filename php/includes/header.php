bd0485b7a829a1ecfd8754ae67fa31a3ec7758b2
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-09-28 23:25:31 -0500

    Update header.php

--- php/includes/header.php
+++ php/includes/header.php
@@ -1,16 +1,10 @@
-<?php 
-    session_start();
-    if(empty($_SESSION['active']))
-	{
-		header('location: ../todos/login.php');
-	}
- ?>
-	<header>
-		<div class="header">
-			<div class="optionsBar">
-                <i class="fa-solid fa-user"></i><span class="user"><?php echo $_SESSION['nombre'].' -'.$_SESSION['email']; ?></span>
-				<a href="../todos/logout.php"><i class="fa-solid fa-right-from-bracket"></i>Salir</a>
-			</div>
+<?php session_start();?>
+<header>
+	<div class="header">
+		<div class="optionsBar">
+			<i class="fa-solid fa-user"></i><span class="user"><?php echo $_SESSION['nombre'].' -'.$_SESSION['email']; ?></span>
+			<a href="../todos/logout.php"><i class="fa-solid fa-right-from-bracket"></i>Salir</a>
 		</div>
-		<?php include "nav.php"; ?>
-	</header>
+	</div>
+	<?php include "nav.php"; ?>
+</header>
