0a9b4c26276c04f47f87206bb37653fe1bd1ab3f
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-10-01 17:58:49 -0500

    Update nav.php

--- php/includes/nav.php
+++ php/includes/nav.php
@@ -11,12 +11,23 @@
                 <li><a href="lista_usuarios.php">Lista de Usuarios</a></li>
             </ul>
         </li>
-    <?php } ?>
         <li class="principal">
             <a href="#">Productos</a>
             <ul>
                 <li><a href="lista_productos.php">Lista de Productos</a></li>
             </ul>
         </li>
+    <?php } ?>
+    <?php
+        if($_SESSION['user'] == 2){
+        ?>
+        <li class="principal">
+
+            <a href="#">Productos</a>
+            <ul>
+                <li><a href="añadir_productos.php">Añador prodictos</a></li>
+            </ul>
+        </li>
+    <?php } ?>
     </ul>
 </nav>
