8d6c1abfe75319af6c1ecdd36cba320b2ae4ca1e
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-10-01 17:58:21 -0500

    Update functions.js

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -38,4 +38,140 @@ $(document).ready(function(){
 
     });
 
+    $('.del_product').click(function(e){
+        e.preventDefault();
+        var producto = $(this).attr('product');
+        var action = 'infoProducto';
+
+        $.ajax({
+            url: '../todos/ajax.php',
+            type: 'POST',
+            async: true,
+            data:{action: action, producto: producto},
+
+            success: function(response) {
+                if (response != 'error') {
+                    var info = JSON.parse(response);
+
+                    $('.bodyModal').html('<form action="" method="post" name="form_del_product" id="form_del_product" onsubmit="event.preventDefault(); delProduct();">'+
+                    '<h1><i class="fas" style="font-size:45pt;"></i><br>Eliminar producto</h1>'+
+                    '<p>¿Estas seguro de eliminar este producto?</p>'+
+                    '<h2 class="nameProducto">'+info.nombre+'</h2><br>'+
+                    '<input type="hidden" name="producto_id" id="producto_id" value="'+info.codproducto+'" required>'+
+                    '<input type="hidden" name="action" id="action" value="delProduct" required>'+
+                    '<div class="alert alertAddProduct"></div>'+
+                    '<a href="#" class="btn_cancel" onclick="coloseModal();"><i class="fa fa-ban"></i>Cerrar</a>'+
+                    '<button type="submit" class="btn_ok"><i class="far fa-trash-alt"></i>Eliminar</button>'+
+                    '</form>');
+                }
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+
+        $('.modal').fadeIn();
+    });
+    
+    $('.del_user').click(function(e){
+        e.preventDefault();
+        var usuario = $(this).attr('user');
+        var action = 'infoUsuario';
+
+        $.ajax({
+            url: '../todos/ajax.php',
+            type: 'POST',
+            async: true,
+            data:{action: action, usuario: usuario},
+
+            success: function(response) {
+                if (response != 'error') {
+                    var info = JSON.parse(response);
+
+                    $('.bodyModal').html('<form action="" method="post" name="form_del_user" id="form_del_user" onsubmit="event.preventDefault(); delUser();">'+
+                    '<h1><i class="fas" style="font-size:45pt;"></i><br>Eliminar usuario</h1>'+
+                    '<p>¿Estas seguro de eliminar este usuario?</p>'+
+                    '<h2 class="nameUsuario">'+info.nombre+'</h2><br>'+
+                    '<input type="hidden" name="usuario_id" id="usuario_id" value="'+info.idusuario+'" required>'+
+                    '<input type="hidden" name="action" id="action" value="delUser" required>'+
+                    '<div class="alert alertAddProduct"></div>'+
+                    '<a href="#" class="btn_cancel" onclick="coloseModal();"><i class="fa fa-ban"></i>Cerrar</a>'+
+                    '<button type="submit" class="btn_ok"><i class="far fa-trash-alt"></i>Eliminar</button>'+
+                    '</form>');
+                }
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+
+        $('.modal').fadeIn();
+    });
+    
 });
+
+function delProduct(){
+    var pr = $('#producto_id').val();
+    $('.alertAddProduct').html('');
+
+    $.ajax({
+        url: '../todos/ajax.php',
+        type: 'POST',
+        async: true,
+        data: $('#form_del_product').serialize(),
+
+        success: function (response) {
+            console.log(response);
+            
+            if (response == 'error') 
+            {
+                $('.alertAddProduct').html('<p style="color: red;">Error al borrar el product</p>');
+            }else{
+                $('.row'+pr).remove();
+                $('#form_del_product .btn_ok').remove();
+                $('.alertAddProduct').html('<p>Producto eliminado correctamente</p>');
+            }
+            
+        },
+
+        error: function(error){
+            console.log(error);
+        
+        }
+    });
+}
+
+function delUser(){
+    var pr = $('#usuario_id').val();
+    $('.alertAddProduct').html('');
+
+    $.ajax({
+        url: '../todos/ajax.php',
+        type: 'POST',
+        async: true,
+        data: $('#form_del_user').serialize(),
+
+        success: function (response) {
+            console.log(response);
+            
+            if (response == 'error') 
+            {
+                $('.alertAddProduct').html('<p style="color: red;">Error al borrar el usuario</p>');
+            }else{
+                $('.row'+pr).remove();
+                $('#form_del_user .btn_ok').remove();
+                $('.alertAddProduct').html('<p>Usuario eliminado correctamente</p>');
+            }
+            
+        },
+
+        error: function(error){
+            console.log(error);
+        
+        }
+    });
+}
+
+function coloseModal(){
+    $('.modal').fadeOut();
+}
