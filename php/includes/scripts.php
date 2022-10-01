ec1207b14ea6e3306852368427aba37832110b00
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-10-01 18:28:24 -0500

    Update scripts.php

--- php/includes/scripts.php
+++ php/includes/scripts.php
@@ -1,5 +1,12 @@
+<link rel="stylesheet" type="text/css" href="../../CSS/Clients/index.css">
+<link rel="stylesheet" type="text/css" href="../../CSS/comen.css">
+<link rel="stylesheet" type="text/css" href="../../CSS/contraseñaperdida.css">
+<link rel="stylesheet" type="text/css" href="../../CSS/editarproducto.css">
+<link rel="stylesheet" type="text/css" href="../../CSS/perfilcompradores.css">
+<link rel="stylesheet" type="text/css" href="../../CSS/registrarProducto.css">
 <link rel="stylesheet" type="text/css" href="../../CSS/style.css">
 <link rel="stylesheet" type="text/css" href="../../CSS/datos.css">
+<link rel="stylesheet" type="text/css" href="../../CSS/Diseño.css">
 <link href="../../fontawesome-free-6.2.0-web/css/fontawesome.css" rel="stylesheet"> 
 <link href="../../fontawesome-free-6.2.0-web/css/brands.css" rel="stylesheet">
 <link href="../../fontawesome-free-6.2.0-web/css/solid.css" rel="stylesheet">
