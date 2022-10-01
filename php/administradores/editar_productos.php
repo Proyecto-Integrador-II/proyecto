b51cd4c17fcbfccdc1efbe2307a49a74b47f7534
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-10-01 00:21:58 -0500

    Update editar_productos.php

--- php/administradores/editar_productos.php
+++ php/administradores/editar_productos.php
@@ -16,68 +16,73 @@
 		}  
 		?>
 		<div class="container">
-			<form class="form-card" id="signup-form" method="POST">
-				<h1 class="form-card__subtitle fas fa-cubes"><i class="fas fa-cubes"></i>Actualizar cuenta</h1>
+			<form class="form-card" method="POST" enctype="multipart/form-data">
+				<h1 class="form-card__subtitle fas">Editar producto</h1>
                 <?php
 				require_once '../todos/conexion.php';
 				if(!empty($_POST))
 				{
-					if(empty($_POST['first-name']) || empty($_POST['last-name']) || empty($_POST['email'])  || empty($_POST['password']))
+					if(empty($_POST['nombre_producto']) || empty($_POST['precio_producto']) || empty($_POST['lugar_producto'])  || empty($_POST['descripcion_producto']) || empty($_POST['foto_producto'])) 
 					{
-						echo 'Todos los campos son obligatorios';
+						$alert='<p class="msg_error">Todos los campos son obligatorios</p>';
 					}
 					else{
+						$nombre_producto = $_POST['nombre_producto'];
+						$precio_producto = $_POST['precio_producto'];
+						$lugar_producto = $_POST['lugar_producto'];
+						$descripcion_producto = $_POST['descripcion_producto'];
+						$foto = $_FILES['foto'];
+						$nombre_foto = $foto['name'];
+						$type = $foto['type'];
+						$url_temp = $foto['tmp_name'];
+						$imgProducto  = 'img_producto.png';
 
-						$idUsuario = $_POST['idUsuario'];
-						$nombre = $_POST['first-name'];
-						$apellido  = $_POST['last-name'];
-						$correo   = $_POST['email'];
-						$contraseña  = $_POST['password'];
+						if($nombre_foto != '')
+						{
+							$destino = '../../img/uploads/';
+							$imgProducto = 'img_'.$nombre_producto.'.png';
+							$src = $destino.$imgProducto;
+						}
+						
 						
 
-						$query = mysqli_query($conection,"SELECT * FROM usuario WHERE (correo = '$correo' AND idusuario != $idUsuario) ");
+						$query_update = mysqli_query($conection,"UPDATE producto SET nombre = '$nombre_producto',precio ='$precio_producto',lugar='$lugar_producto',foto='$imgProducto' WHERE codproducto = $codproducto;");
 
-						$result = mysqli_fetch_array($query);
-
-						if($result > 0){
-							echo 'El correo o el usuario ya existe';
-						}else{				
-							$sql_update = mysqli_query($conection,"UPDATE usuario SET nombre = '$nombre', apellido = '$apellido',correo='$correo',clave='$contraseña' WHERE idusuario= $idUsuario ");
-
-							if($sql_update)
+						if($sql_update)
+						{
+							if($nombre_foto != '')
 							{
-								echo 'Usuario actualizado correctamente';
-							}else
-							{
-								echo 'Error al actualizar el usuario';
+								move_uploaded_file($url_temp,$src);
 							}
+							echo 'Producto actualizado correctamente';
+						}else
+						{
+							echo 'Error al actualizar el producto';
 						}
+						
 					}
 				}
                 if(empty($_GET['id']))
                 {
-					
+					header('Location: lista_productos.php');
                 }
 
-                $iduser = $_GET['id'];
-                $sql = mysqli_query($conection,"SELECT u.idusuario, u.nombre, u.apellido, u.correo, (u.rol) as idrol, (r.rol) as rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE idusuario = $iduser");
-                $result_sql = mysqli_num_rows($sql);
+                $codproducto = $_GET['id'];
+                $query_producto = mysqli_query($conection,"SELECT p.codproducto, p.nombre AS producto, p.precio, l.nombre AS lugar,  p.descripcion, p.foto, u.nombre, l.id FROM producto p INNER JOIN usuario u ON p.proveedor = u.idusuario INNER JOIN lugares l ON l.id = p.lugar WHERE codproducto = $codproducto AND p.habilitado = 1;");
+                $result_producto = mysqli_num_rows($query_producto);
+				$foto = '';
+				$classRemove = 'notBlock';
 
-                if($result_sql == 0)
+                if($result_producto > 0)
                 {
-                    
+                    $data_producto = mysqli_fetch_assoc($query_producto);
 					
-                }
+					$classRemove = '';
+					$foto = '<img id="img" src="../../img/uploads/'.$data_producto['foto'].'" alt="Producto">';
+				}	
  
                 else{
-                    while($data = mysqli_fetch_array($sql)){
-                        $iduser = $data['idusuario'];
-                        $nombre = $data['nombre'];
-                        $apellido = $data['apellido'];
-                        $correo = $data['correo'];
-                        $idrol = $data['idrol'];
-                        $rol = $data['rol'];
-                    }
+                    header('location: lista_productos.php');
 					
                 }
                 ?>
@@ -87,94 +92,95 @@
 					<input
 						class="form-card__input"
 						type="hidden"
-						name="idUsuario"
-                        value="<?php echo $iduser; ?>"
+						name="codproducto"
+                        value="<?php echo $data_producto['codproducto']; ?>"
 					/>
 					
 				</div>
 
 				<div class="form-card__group">
-					<label class="form-card__label" for="first-name">Nombre:</label>
+					<label class="form-card__label"></label>
 					<input
 						class="form-card__input"
-						type="text"
-						name="first-name"
-						id="first-name"
-						placeholder="Nombres"
-                        value="<?php echo $nombre;?>"
+						type="hidden"
+						name="foto_actual"
+						id="foto_actual"
+                        value="<?php echo $data_producto['foto']; ?>"
 					/>
+					
 				</div>
 
 				<div class="form-card__group">
-					<label class="form-card__label" for="last-name">Apellido:</label>
+					<label class="form-card__label"></label>
 					<input
 						class="form-card__input"
-						type="text"
-						name="last-name"
-						id="last-name"
-						placeholder="Apellidos"
-                        value="<?php echo $apellido;?>"
+						type="hidden"
+						name="foto_remove"
+						id="foto_remove"
+                        value="<?php echo $data_producto['foto']; ?>"
 					/>
-					<p class="form-card__error">
-						Los apellildos solo puede contener de 5 a 40 letras.
-					</p>
+					
 				</div>
 
 				<div class="form-card__group">
-					<label class="form-card__label" for="email">Email:</label>
+					<label class="form-card__label" for="nombre_producto">Nombre:</label>
 					<input
 						class="form-card__input"
 						type="text"
-						name="email"
-						id="email"
-						placeholder="Email"
-                        value="<?php echo $correo;?>"
+						name="nombre_producto"
+						id="nombre_producto"
+						placeholder="Nombre del producto"
+						value="<?php echo $data_producto['producto'];?>"
 					/>
-					<p class="form-card__error">
-						*El correo debe tener la forma username@domain.com
-					</p>
-				</div>
-				<div class="form-card__group">
-					<label class="form-card__label" for="password">Contraseña:</label>
-					<input
-						class="form-card__input"
-						type="password"
-						name="password"
-						id="password"
-						placeholder="Contraseña"
-					/>
-					<p class="form-card__error">
-						*La contraseña debe tener mínimo 8 caracteres, una letra, un número y uno de
-						los siguientes caracteres especiales: $ ! % * # ? & / %
-					</p>
-				</div>
-				<div class="form-card__group">
-					<label class="form-card__label" for="password-2"
-						>Confirmación contraseña:</label
-					>
-					<input
-						class="form-card__input"
-						type="password"
-						name="password-2"
-						id="password-2"
-						placeholder="Repetir contraseña"
-					/>
-					<p class="form-card__error">Ambas contraseñas deben ser iguales.</p>
 				</div>
 
-				<div class="cl"></div>
+                <div class="form-card__group">
+					<label class="form-card__label" for="precio_producto">Precio:</label>
+					<input
+						class="form-card__input"
+						type="number"
+						name="precio_producto"
+						id="precio_producto"
+						placeholder="Precio del producto"
+						value="<?php echo $data_producto['precio']; ?>"
+					/>
+				</div>
+
+                <div class="form-card__group">
+					<label class="form-card__label" for="lugar_producto">Lugar:</label>
+					<select name="lugar_producto" class="notItemOne">
+						<option value="<?php echo $data_producto['id']; ?>" selected><?php echo $data_producto['lugar']; ?></option>
+                        <option value="1">UPB</option>
+                        <option value="2">Delacuesta</option>
+                        <option value="3">Parque caracoli</option>
+                    </select>
+				</div>
+
+                <div class="form-card__group">
+					<label class="form-card__label" for="descripcion_producto">Descripcion:</label>
+                    <textarea id="descripcion_producto" name="descripcion_producto" rows="4" cols="50" ><?php echo htmlspecialchars($data_producto['descripcion']); ?></textarea>
+				</div>
+
+                <div class="form-card__group">
+					<label class="form-card__label" for="foto">Foto:</label>
+					<div class="prevPhoto">
+                        <span class="delPhoto <?php echo $classRemove; ?>">X</span>
+                        <label for="foto"></label>
+						<?php echo $foto; ?>
+                    </div>
+                    <div class="upimg">
+                        <input type="file" name="foto" id="foto">
+                    </div>
+                    <div id="form_alert"></div>
+                 
+                </div>
 
 				<div class="form-card__buttons">
-					<button class="btn btn--green btn--block" type="submit" name="crear">Actualizar</button>
+					<button class="btn btn--green btn--block" type="submit" name="crear">Añadir producto</button>
 				</div>
-
-				<p class="form-card__error" id="form-error">
-					Rellene todos los campos del formulario correctamente antes de realizar el
-					envío.
-				</p>
 			</form>
 		</div>
-		<script src="../assets/javascript/dom.js" type="module"></script>
-		<script src="../assets/javascript/validations/signup.js"></script>
+        <script src="../JavaScript/jquery.min.js"></script>
+        <script src="../JavaScript/functions.js"></script>
 	</body>
 </html>
