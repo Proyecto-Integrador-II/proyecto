9629e7194b353055fa78ee6c92ff7ab81c5fde8c
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-10-02 15:36:16 -0500

    Update Contraseñaperdida.php

--- php/todos/Contraseñaperdida.php
+++ php/todos/Contraseñaperdida.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/contraseñaperdida.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-    <?php include '../includes/scripts.php' ?>
+    <link rel="stylesheet" href="../../CSS/contraseñaperdida.css">    
     <title>Contraseña-Perdida</title>
 </head>
 <body>
