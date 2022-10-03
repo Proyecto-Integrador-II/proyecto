8c29191aaa2d90f1ce7f22f94f55ffc091285f2d
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-10-02 19:45:07 -0500

    Update buscar_productos.php

--- php/administradores/buscar_productos.php
+++ php/administradores/buscar_productos.php
@@ -17,40 +17,82 @@
 		?>
         <section id="container">
             <?php
-                $busqueda = strtolower($_REQUEST['busqueda']);
-                if(empty($busqueda))
+                $busqueda = '';
+				$search_proveedor = '';
+				$lugar = '';
+                if(empty($_REQUEST['busqueda']) && empty($_REQUEST['proveedor']))
                 {
-                    header('Location: ./lista_usuarios.php');
+                    header('Location: ./lista_productos.php');
                 }
+				if($busqueda == 'UPB'){
+					$lugar = " OR p.lugar LIKE '%1%' ";
+				}else if($busqueda == 'Delacuesta'){
+					$lugar = " OR p.lugar LIKE '%2%' ";
+				}else if($busqueda == 'Parque caracoli'){
+					$lugar = " OR p.lugar LIKE '%3%' ";
+				}
+				if(!empty($_REQUEST['busqueda'])){
+					$busqueda = strtolower($_REQUEST['busqueda']);
+					$where = "(p.codproducto LIKE '%$busqueda%' OR p.nombre LIKE '%$busqueda%' OR p.precio LIKE '%$busqueda%' $lugar  OR p.descripcion LIKE '%$busqueda%') AND p.habilitado =1";
+					$buscar = 'busqueda='.$busqueda;
+				}
+				if(!empty($_REQUEST['proveedor'])){
+					$search_proveedor = strtolower($_REQUEST['proveedor']);
+					$where = "p.proveedor LIKE $search_proveedor AND p.habilitado =1";
+					$buscar = 'proveedor='.$search_proveedor;
+				}
             ?>
-            <h1><i class="fas fa-user"></i>Lista de Usuarios</h1>
-			<form action="buscar_usuarios.php" method="GET" class="form_search">
+            <h1><i class="fas fa-user"></i>Lista de productos</h1>
+			<form action="buscar_productos.php" method="GET" class="form_search">
 				<input type="text" name="busqueda" id="busqueda" placeholder="Buscar" value="<?php echo $busqueda; ?>">
 				<input type="submit" value="Buscar" class="btn_search"><i class="fas fa-search"></i>
 			</form>
 
 			
             <table>
-                <tr>
+				<tr>
                     <th>ID</th>
                     <th>Nombre</th>
-                    <th>Apellido</th>
-                    <th>Correo</th>
-                    <th>Rol</th>
-                    <th>Acciones</th>
+                    <th>Precio</th>
+                    <th>Lugar</th>
+                    <th>Descripcion</th>
+                    <th>Foto</th>
+                    <th>	
+					<?php 
+						require_once '../todos/conexion.php';
+						$pro = 0;
+						if(!empty($_REQUEST['proveedor'])) {
+							$pro = $_REQUEST['proveedor'];
+						}
+						$query_proveedor = mysqli_query($conection, "SELECT * FROM usuario where habilitado =1 and rol = 2 order by idusuario ASC");
+						$result_proveedor = mysqli_num_rows($query_proveedor);
+					?>
+					<select name="proveedor" id="search_proveedor">
+						<option value="" selected>Proveedor</option>
+						<?php 
+							if($result_proveedor >0)
+							{
+								while ($proveedor = mysqli_fetch_array($query_proveedor)) { 
+									if($pro ==$proverdor['idusuario'])
+										{
+						?>
+								<option value="<?php echo $proveedor['idusuario']; ?>" selected><?php echo $proveedor['nombre'] ?></option>
+						<?php
+									}else{
+						?>
+								<option value="<?php echo $proveedor['idusuario']; ?>"><?php echo $proveedor['nombre'] ?></option>
+							}
+						<?php
+									}
+								}
+							}
+						?>
+					</select>
+					</th>
+					<th>Acciones</th>
                 </tr>
             <?php
-				require_once '../todos/conexion.php';
-				$rol = '';
-				if($busqueda == 'Administradores')
-				{
-					$rol = " OR rol LIKE '%1%' ";
-				}else if($busqueda == 'Vendedores'){
-					$rol = " OR rol LIKE '%2%' ";
-				}else if($busqueda == 'Compradores'){
-					$rol = " OR rol LIKE '%3%' ";
-				}
-				$sql_registe = mysqli_query($conection,"SELECT COUNT(*) AS total_registro FROM usuario WHERE (idusuario LIKE '%$busqueda%' OR nombre LIKE '%$busqueda%' OR apellido LIKE '%$busqueda%' OR correo LIKE '%$busqueda%' $rol) AND habilitado =1");
+				$sql_registe = mysqli_query($conection,"SELECT COUNT(*) AS total_registro FROM producto as p WHERE $where");
 				
 				$result_register = mysqli_fetch_array($sql_registe);
 				$total_registro = $result_register['total_registro'];
@@ -62,7 +104,7 @@
 				}
 				$desde = ($pagina-1)*$por_pagina;
 				$total_paginas = ceil($total_registro/$por_pagina);
-                $query = mysqli_query($conection, "SELECT u.idusuario, u.nombre,u.apellido,u.correo,r.rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol where (u.idusuario LIKE '%$busqueda%' OR u.nombre LIKE '%$busqueda%' OR u.correo LIKE '%$busqueda%' OR r.rol LIKE '%$busqueda%') AND habilitado =1 order by u.idusuario ASC LIMIT $desde,$por_pagina");
+                $query = mysqli_query($conection, "SELECT p.codproducto as ID,p.nombre as Nombre,p.precio as Precio,l.nombre as Lugar,p.descripcion AS Descripcion,p.foto as Foto,u.nombre as Proveedor FROM producto p INNER JOIN lugares l ON p.lugar = l.id INNER JOIN usuario u ON u.idusuario = p.proveedor where p.habilitado =1 ORDER BY p.codproducto ASC limit $desde,$por_pagina");
                 $result = mysqli_num_rows($query);
 
                 if($result > 0)
@@ -71,18 +113,18 @@
                     {
                 ?>
                     <tr>
-                        <td><?php echo $data['idusuario'] ?></td>
-                        <td><?php echo $data['nombre'] ?></td>
-                        <td><?php echo $data['apellido'] ?></td>
-                        <td><?php echo $data['correo'] ?></td>
-                        <td><?php echo $data['rol'] ?></td>
-                        <td>
-                        
-						<?php if($data['rol'] != 'Administradores'){ ?>
-							<a class="link_edit" href="./editar_usuarios.php?id=<?php echo $data['idusuario'];?>">Editar</a>
-                            <a class="link_delete" href="./eliminar_usuarios.php?id=<?php echo $data['idusuario'];?>">Eliminar</a>
-						<?php } ?>
+                        <td><?php echo $data['ID'] ?></td>
+                        <td><?php echo $data['Nombre'] ?></td>
+                        <td><?php echo $data['Precio'] ?></td>
+                        <td><?php echo $data['Lugar'] ?></td>
+                        <td><?php echo $data['Descripcion'] ?></td>
+						<td class="img_producto"><img src="<?php echo $foto; ?>" alt="<?php echo $data['Descripcion']; ?>"></td>
+						<td><?php echo $data['Proveedor'] ?></td>
+						<td>
+						<a class="link_edit" href="./editar_productos.php?id=<?php echo $data['ID'];?>">Editar</a>
+                        <a class="link_delete" href="./eliminar_usuarios.php?id=<?php echo $data['ID'];?>">Eliminar</a>
                         </td>
+						
                     </tr>
             <?php
                     }
@@ -99,8 +141,8 @@
 					if($pagina !=1)
 					{
 				?>
-					<li><a href="?pagina=<?php echo 1; ?>&busqueda=<?php echo $busqueda; ?>">|<</a></li>
-					<li><a href="?pagina=<?php echo $pagina-1; ?>&busqueda=<?php echo $busqueda; ?>"><<</a></li>
+					<li><a href="?pagina=<?php echo 1; ?>&<?php echo $buscar; ?>">|<</a></li>
+					<li><a href="?pagina=<?php echo $pagina-1; ?>&<?php echo $buscar; ?>"><<</a></li>
 				<?php
 					}
 					for ($i=1;$i<$total_paginas;$i++){
@@ -108,14 +150,14 @@
 						{
 							echo '<li class="pageSelected">'.$i.'</li>';
 						}else{
-							echo '<li><a href="?pagina='.$i.'$busqueda='.$busqueda.'">'.$i.'</a></li>';
+							echo '<li><a href="?pagina='.$i.'&'.$buscar.'">'.$i.'</a></li>';
 						}
 					}
 					if($pagina != $total_paginas)
 					{
 				?>
-					<li><a href="?pagina=<?php echo $pagina+1; ?>$busqueda=<?php echo $busqueda; ?>">>></a></li>
-					<li><a href="?pagina=<?php echo $total_paginas; ?>$busqueda=<?php $busqueda; ?>">>|</a></li>
+					<li><a href="?pagina=<?php echo $pagina+1; ?>&<?php echo $buscar; ?>">>></a></li>
+					<li><a href="?pagina=<?php echo $total_paginas; ?>&<?php echo $buscar; ?>">>|</a></li>
 				<?php } ?>
 				</ul>
 			</div>
