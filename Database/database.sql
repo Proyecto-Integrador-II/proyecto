5bb7e311409513a95b3b313de522f4e161ad3401
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-10-04 17:05:08 -0500

    Update database.sql

--- Database/database.sql
+++ Database/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3307
--- Tiempo de generación: 03-10-2022 a las 17:22:46
+-- Tiempo de generación: 04-10-2022 a las 23:54:58
 -- Versión del servidor: 10.6.7-MariaDB
 -- Versión de PHP: 8.1.1
 
@@ -61,6 +61,41 @@ CREATE TABLE `producto` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `razones_reporte`
+--
+
+CREATE TABLE `razones_reporte` (
+  `id` int(11) NOT NULL,
+  `tipo_reporte` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Volcado de datos para la tabla `razones_reporte`
+--
+
+INSERT INTO `razones_reporte` (`id`, `tipo_reporte`) VALUES
+(1, 'Irrespeto con el vendedor'),
+(2, 'No pagó'),
+(3, 'No se encontraba en el lugar de pedido'),
+(4, 'Canceló el servicio despues de haberse comenzado el proceso');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reporte`
+--
+
+CREATE TABLE `reporte` (
+  `id` int(11) NOT NULL,
+  `id_razon` int(11) NOT NULL,
+  `id_reportado` int(11) NOT NULL,
+  `reporte` varchar(1000) NOT NULL,
+  `foto` text NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `rol`
 --
 
@@ -86,14 +121,23 @@ INSERT INTO `rol` (`idrol`, `rol`) VALUES
 
 CREATE TABLE `usuario` (
   `idusuario` int(11) NOT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
-  `apellido` varchar(50) DEFAULT NULL,
-  `correo` varchar(100) DEFAULT NULL,
-  `clave` varchar(100) DEFAULT NULL,
-  `rol` int(11) DEFAULT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `clave` varchar(100) NOT NULL,
+  `rol` int(11) NOT NULL,
   `habilitado` tinyint(1) NOT NULL DEFAULT 1,
   `calificacion` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idusuario`, `nombre`, `apellido`, `correo`, `clave`, `rol`, `habilitado`, `calificacion`) VALUES
+(1, 'jean', 'parra', 'jean@administrador.com', '1234', 1, 1, NULL),
+(2, 'jean', 'parra', 'jean@vendedor.com', '1234', 2, 1, NULL),
+(3, 'jean', 'parra', 'jean@comprador.com', '1234', 3, 1, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -113,6 +157,20 @@ ALTER TABLE `producto`
   ADD PRIMARY KEY (`codproducto`),
   ADD KEY `proveedor` (`proveedor`),
   ADD KEY `lugar` (`lugar`);
+
+--
+-- Indices de la tabla `razones_reporte`
+--
+ALTER TABLE `razones_reporte`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reporte`
+--
+ALTER TABLE `reporte`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_razon` (`id_razon`),
+  ADD KEY `id_reportado` (`id_reportado`);
 
 --
 -- Indices de la tabla `rol`
@@ -144,6 +202,18 @@ ALTER TABLE `producto`
   MODIFY `codproducto` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `razones_reporte`
+--
+ALTER TABLE `razones_reporte`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reporte`
+--
+ALTER TABLE `reporte`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
@@ -165,6 +235,13 @@ ALTER TABLE `usuario`
 ALTER TABLE `producto`
   ADD CONSTRAINT `FK_producto_lugares` FOREIGN KEY (`lugar`) REFERENCES `lugares` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`proveedor`) REFERENCES `usuario` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `reporte`
+--
+ALTER TABLE `reporte`
+  ADD CONSTRAINT `FK_reporte_razones_reporte` FOREIGN KEY (`id_razon`) REFERENCES `razones_reporte` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_reporte_usuario` FOREIGN KEY (`id_reportado`) REFERENCES `usuario` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `usuario`
