2431812216103d20e735d46c6bc02f0a3d6ceaf2
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-10-19 20:44:38 -0500

    Update recomendaciones.php

--- php/compradores/recomendaciones.php
+++ php/compradores/recomendaciones.php
@@ -26,18 +26,22 @@
 
         foreach($html->find('div[class="css-t5hujq"]') as $comida) {
             $nombre = $comida->find('div[class="css-k008qs"] p',0);
-            $descripcion = $comida->find('p[class="chakra-text sc-f7e281dd-2 TTnoE css-1rmjo0r"]');
+            $descripcion = $comida->find('p[class="chakra-text sc-f7e281dd-2 TTnoE css-1rmjo0r"]',0);
             $precio = $comida->find('div[class="css-0"] span');
-            $foto = $comida->find('div[class="sc-34e632bd-0 cxugzC sc-f7e281dd-1 knCjyr"] img');
-            $descripcion_post = implode($descripcion);
+            $foto = $comida->find('div[class="sc-34e632bd-0 cxugzC sc-f7e281dd-1 knCjyr"] img',1);
+            $descripcion_post = ($descripcion);
             $precio_post = implode($precio);
-            $foto_post = implode($foto);
+            $foto_post = ($foto);
             
             echo "
             <article id='buscar' class='product' data-id=''>
-                <img id='buscar' class='product-img' alt='' src=''>
-                <h3  id='buscar' class='product-title'>$nombre</h3>
-                <p id='buscar'  class='price'></p>
+                <div class='product__image'>
+                $foto_post 
+                </div>
+                
+                <h3  id='buscar' class='product__title'>$nombre</h3>
+                <p id='buscar'  class='product__price'></p>
+                <p id='buscar'  class='product__amount'>$descripcion_post</p>
                 <button id='buscar' class='btn btn--orange btn--block product__cart-button' data-product-id='' type='button'>
                     <i id='buscar' class='fa-solid fa-basket-shopping cart-add' id='carrito' >Carrito</i>
                 </button>
