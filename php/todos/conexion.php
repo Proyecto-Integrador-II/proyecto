e44694638e1da5927d4ec1fb50d40ac07e665d32
Jean-Parra <jean.parra.2020@upb.edu.co>
2022-10-19 20:48:17 -0500

    Merge branch 'main' of https://github.com/Proyecto-Integrador-II/proyecto

--- php/todos/conexion.php
+++ php/todos/conexion.php
@@ -1,12 +1,16 @@
 <?php
-    $host = 'localhost:3307';
-    $user = 'root';
-    $password = 'upb2021';
-    $db = 'proyecto2';
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conection = new PDO("sqlsrv:server = tcp:pi-servidor.database.windows.net,1433; Database = PI-database", "administrador", "Jeanparra17@");
+    $conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-    $conection = @mysqli_connect($host,$user,$password,$db);
-
-    if(!$conection){
-        echo "Error en la conexión";
-    }
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "administrador", "pwd" => "Jeanparra17@", "Database" => "PI-database", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:pi-servidor.database.windows.net,1433";
+$conection = sqlsrv_connect($serverName, $connectionInfo);
 ?>
