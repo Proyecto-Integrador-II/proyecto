3d66a6e3ba793d44661c23c23a1e5b5b5a22b855
Jean-Parra <jean.parra.2020@upb.edu.co>
2022-10-19 21:55:14 -0500

    Update conexion.php

--- php/todos/conexion.php
+++ php/todos/conexion.php
@@ -1,16 +1,12 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conection = new PDO("sqlsrv:server = tcp:pi-servidor.database.windows.net,1433; Database = PI-database", "administrador", "Jeanparra17@");
-    $conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+    $host = 'localhost:3307';
+    $user = 'root';
+    $password = 'upb2021';
+    $db = 'proyecto2';
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "administrador", "pwd" => "Jeanparra17@", "Database" => "PI-database", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:pi-servidor.database.windows.net,1433";
-$conection = sqlsrv_connect($serverName, $connectionInfo);
+    $conection = @mysqli_connect($host,$user,$password,$db);
+
+    if(!$conection){
+        echo "Error en la conexión";
+    }
 ?>
