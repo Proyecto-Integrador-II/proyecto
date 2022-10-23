e5696bdc34dbbd76d8f2fdc48c4f765d2723ff01
Jean-Parra <jean.parra.2020@upb.edu.co>
2022-10-23 15:39:18 -0500

    Update recuperar.php

--- php/todos/recuperar.php
+++ php/todos/recuperar.php
@@ -3,12 +3,12 @@ include('conexion.php');
 
 $correo = $_POST ['txtcorreo'];
 
-$queryusuario = mysqli_query($coon, "SELECT * FROM lofin WHERE correo = '$correo'");
-$nr           =mysqli_num_rows($queryusuario);
+$queryusuario = sqlsrv_query($coon, "SELECT * FROM lofin WHERE correo = '$correo'");
+$nr           =sqlsrv_num_rows($queryusuario);
 
 if($nr == 1)
 {
-    $mostrar  = mysqli_fetch_array($queryusuario);
+    $mostrar  = sqlsrv_fetch_array($queryusuario);
     $enviarpass = $mostrar ['password'];
 
     $paracorreo = $correo;
