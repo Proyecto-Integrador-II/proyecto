1e2ce05fd9dfff2d5c8b5e5ed4b46f88f9c7d894
Jean-Parra <jean.parra.2020@upb.edu.co>
2022-10-23 15:39:29 -0500

    Update lista_compradores.php

--- php/vendedores/lista_compradores.php
+++ php/vendedores/lista_compradores.php
@@ -32,8 +32,8 @@
             <?php
 				require_once '../todos/conexion.php';
 				
-				$sql_registe = mysqli_query($conection,"SELECT COUNT(*) AS total_registro FROM usuario WHERE rol = 3 and habilitado =1");
-				$result_register = mysqli_fetch_array($sql_registe);
+				$sql_registe = sqlsrv_query($conection,"SELECT COUNT(*) AS total_registro FROM usuario WHERE rol = 3 and habilitado =1");
+				$result_register = sqlsrv_fetch_array($sql_registe);
 				$total_registro = $result_register['total_registro'];
 				$por_pagina = 50;
 				if(empty($_GET['pagina'])){
@@ -43,13 +43,12 @@
 				}
 				$desde = ($pagina-1)*$por_pagina;
 				$total_paginas = ceil($total_registro/$por_pagina);
-                $query = mysqli_query($conection, "SELECT u.idusuario, u.nombre,u.apellido,u.correo FROM usuario u where rol = 3 and habilitado =1 ORDER BY u.idusuario ASC limit $desde,$por_pagina");
-                mysqli_close($conection);
-				$result = mysqli_num_rows($query);
+                $query = sqlsrv_query($conection, "SELECT u.idusuario, u.nombre,u.apellido,u.correo FROM usuario u where rol = 3 and habilitado =1 ORDER BY u.idusuario OFFSET $desde ROWS FETCH NEXT $por_pagina ROWS ONLY");
+				$result = sqlsrv_num_rows($query);
 
-                if($result > 0)
+                if($result === false)
                 {
-                    while ($data = mysqli_fetch_array($query))
+                    while ($data = sqlsrv_fetch_array($query))
                     {
                 ?>
                     <tr>
